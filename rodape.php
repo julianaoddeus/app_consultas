3343a2af65b12c63294114820c7cc41449872308
Juliana <julianaacademi@gmail.com>
2021-05-13 19:23:30 -0300

    Update index,cabeçalho e rodapé

---
+++ rodape.php
@@ -0,0 +1,9 @@
+
+<footer>
+            &copy; Todos os direitos reservados
+        </footer>
+    </main>
+    <script type="js/bootstrap-js/bootstrap.bundle.js"></script>
+    <script src="js/jquery.js"></script>
+</body>
+</html>
