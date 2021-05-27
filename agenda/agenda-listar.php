a9c3a3db8b138ef2a5795299276fe7e37b5b1e2f
Judeoddeus <64862169+Judeoddeus@users.noreply.github.com>
2021-05-26 21:29:42 -0300

    criação do filtro por médicos em agenda-listar
    erro de atualização por terminal

--- agenda/agenda-listar.php
+++ agenda/agenda-listar.php
@@ -1,12 +1,36 @@
-<?php include "../includes/cabecalho.php";?>
+<?php include "../includes/cabecalho.php"; include "../includes/conexao.php";?>
 
 <hr>
 
 <p>
     <a href="agenda-formulario-inserir.php" class="btn btn-danger bi-plus-circle">Agendamento</a>
 </p>
+<hr><br>
+<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+  <div class="row">
+    <div class="col-1">Médico:</div>
+    <div class="col-3">
+      <select name="id_medico" class="form-select">
+        <option value="todos">TODOS</option>
+        <?php
+          $sqlBuscaMedicos = "SELECT * FROM tb_medicos";
+          $listaDeMedicos = mysqli_query($conexao , $sqlBuscaMedicos);
+
+          while($medico = mysqli_fetch_array($listaDeMedicos)){
+              echo "<option value='{$medico['id']}'>";
+              echo $medico['nome'];
+              echo "</option>";
+          }
+        ?>
+        </select>        
+    </div>
+    <div class="col-4">
+          <button class="btn btn-danger"><i class="bi bi-funnel" title="filtrar consultas"></i>BUSCAR</button>
+    </div>
+  </div>
+</form>
 <?php 
-include "../includes/conexao.php";
+
 
 $sqlBusca = "SELECT 
                 a.id, 
@@ -19,11 +43,17 @@ $sqlBusca = "SELECT
             INNER JOIN tb_pacientes p ON a.id_paciente = p.id 
             INNER JOIN tb_medicos m ON a.id_medico = m.id";
 
+if(isset($_POST['id_medico'])){
+  if($_POST['id_medico'] != 'todos'){ 
+    $sqlBusca .= " WHERE a.id_medico = ". $_POST['id_medico'];
+  }
+}
+
 $listaAgenda = mysqli_query($conexao, $sqlBusca);
 
 ?>
 
-
+<br>
 <table class="table">
 <thead class="border">
     <tr>
